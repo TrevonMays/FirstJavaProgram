933e11b25d8b1402aab687c89571305f964802c5
Trevon Mays <103228576+TrevonMays@users.noreply.github.com>
2022-09-27 14:05:46 -0700

    Add files via upload

---
+++ src/FirstJavaProgram.java
@@ -0,0 +1,6 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        Utils trey = new Utils();
+        trey.printMessage();
+    }
+}
---
+++ src/Utils.java
@@ -0,0 +1,12 @@
+
+public class Utils {
+    public void printMessage(){
+        String name = "Coding Dojo\n";
+        String hometown = "Stone Mountain, Ga\n";
+        int age = 100;
+        System.out.format("My name is %s\n %s is %d years old\n My hometown is %s",name,name,age, hometown);
+//        System.out.format("I am %d years old.\n",age);
+//        System.out.println(hometown);
+
+    }
+}
